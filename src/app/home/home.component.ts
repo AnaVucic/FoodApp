e44ff6fabbcd5fa379346b8fd9370814ed184935
AnaVucic <anavucic007@gmail.com>
2023-02-25 13:54:32 +0100

    food service and home components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../services/food/food.service';
+import { Food } from '../shared/models/Food';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  foods: Food[] = [];
+
+  constructor(private foodServise: FoodService) { }
+
+  ngOnInit(): void {
+    this.foods = this.foodServise.getAll();
+  }
 }
---
+++ src/app/services/food/food.service.ts
@@ -0,0 +1,83 @@
+import { Injectable } from '@angular/core';
+import { Food } from 'src/app/shared/models/Food';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodService {
+
+  constructor() { }
+
+  getAll(): Food[] {
+    return [
+      {
+        id: 1,
+        name: 'Oatmeal Bowl',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins: ['qsia', 'europe'],
+        stars: 4.5,
+        imageUrl: '/assets/images/foods/food-1.jpg',
+        tags: ['Healthy', 'Breakfast', 'Fruit', 'Sweet', 'Vegan'],
+      },
+      {
+        id: 2,
+        name: 'Hummus dip',
+        price: 20,
+        cookTime: '20-30',
+        favorite: true,
+        origins: ['egypt', 'middle east'],
+        stars: 4.7,
+        imageUrl: '/assets/images/foods/food-2.jpg',
+        tags: ['Dip', 'Healty', 'Side Dish', 'Vegan'],
+      },
+      {
+        id: 3,
+        name: 'Steak',
+        price: 35,
+        cookTime: '20-30',
+        favorite: false,
+        origins: ['italy'],
+        stars: 4.0,
+        imageUrl: '/assets/images/foods/food-3.jpg',
+        tags: ['Dinner', 'Meat'],
+      },
+      {
+        id: 4,
+        name: 'Pasta',
+        price: 20,
+        cookTime: '25-35',
+        favorite: true,
+        origins: ['italy'],
+        stars: 4.9,
+        imageUrl: '/assets/images/foods/food-4.jpg',
+        tags: ['Lunch'],
+      },
+      {
+        id: 5,
+        name: 'Udon noodles',
+        price: 16,
+        cookTime: '40-50',
+        favorite: false,
+        origins: ['japan'],
+        stars: 4.2,
+        imageUrl: '/assets/images/foods/food-5.jpg',
+        tags: ['SlowFood', 'Soup', 'Vegetables', 'Meat'],
+      },
+      {
+        id: 6,
+        name: 'Pancakes',
+        price: 9,
+        cookTime: '15-25',
+        favorite: false,
+        origins: ['america'],
+        stars: 4.0,
+        imageUrl: '/assets/images/foods/food-6.jpg',
+        tags: ['FastFood', 'Sweet'],
+      },
+
+    ]
+  }
+
+}
